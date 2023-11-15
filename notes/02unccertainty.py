7e12527baf8d6ad1fbc625bd4cfa11c1054066e7
Kristian Padilla <ketpadilla05@gmail.com>
2023-11-15 20:05:54 +0800

    l2s1

---
+++ notes/02unccertainty.py
@@ -0,0 +1,35 @@
+
+### * UNCERTAINTY
+## Used in AI to make inferences with incomplete information by using probaability.
+
+## * PROBABILITY THEORY
+# A branch of mathematics that deals with calculating the likelihood of events.
+'''
+    States that for a given event, there are many possible worlds (w), where each world has a probability (P(w)).
+        - Every probability is between 0 and 1:     0 ≤ P(w) ≤ 1 (inclusive)
+            1 - Certain
+            0 - Impossible
+        - The sum of all probabilities is 1:        ∑ P(w) = 1
+
+        For a pair of events A and B:
+            - each event may or may not have the same probability with other events
+            - however, the sum of the probabilities will never be the same with other pairs of events
+
+    UNCONDITIONAL PROBABILITY
+    the degree of belief in a proposition (P(A)) without any other information
+    calculates the probability of an event A by dividing the number of ways event A can occur by the total number of possible events
+
+
+    CONDITIONAL PROBABILITY
+    the degree of belief in a proposition (P(A|B)) given some other information (B) that is known
+        - P(A|B) is read as "the probability of A given B"
+
+        formula:
+            P(A|B) = P(A ^ B) / P(B)
+    
+    RANDOM VARIABLE
+    a variable in probability theory with a domain of possible values it can take on
+'''
+
+#! 17:20
+
