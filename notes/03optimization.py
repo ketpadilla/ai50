2b5ba60a86eba4d73594be2828f6e95bb8503a47
Kristian Padilla <ketpadilla05@gmail.com>
2023-11-20 23:31:43 +0800

    l3s1

---
+++ notes/03optimization.py
@@ -0,0 +1,25 @@
+
+### * OPTIMIZATION
+## Choosing the best option from a set of options
+
+## * LOCAL SEARCH
+# a search algorithm that only maintains a single node and searches by moving to a neighboring node
+'''
+    it does not simultaneously explore multiple paths
+
+    useful for problems where only the solution matters 
+'''
+
+## * STATE SPACE LANDSCAPE
+# a visualization (diagram) of the cost (in vertical bars) of each state in a search problem
+'''
+    calculated by a heuristic function
+    
+    can be used to determine the global maximum/minimum of an objective/cost function
+'''
+
+##! Strategy for Local Search
+# * take a state, maintaining some current node, and move in a state-space landscape to find a global maximum/minimum
+
+## * HILL CLIMBING
+#! 7:35
