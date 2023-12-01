22d6aa9fd5d9921235a09fc6dd5f9bed601179e9
Kristian Padilla <ketpadilla05@gmail.com>
2023-12-01 17:29:39 +0800

    l5s1

---
+++ notes/05neuralNetworks.py
@@ -0,0 +1,24 @@
+
+### * NEURAL NETWORKS
+## * ARTIFICIAL NEURAL NETWORK
+# a mathematical model for learning inspired by biological neural networks
+'''
+  models a mathematical function mappping inputs to outputss based on the network's structure and parameters
+
+  ACTIVATION FUNCTION
+  function calculating a node's output (refer to learning algorithms)
+    e.g., 
+      1. Step Function
+        g(x) = 1 if x ≥ 0, else 0
+
+      2. Logistic Sigmoid
+        g(x) = e^x / (e^x + 1)
+
+      3. Rectified Linear Unit (ReLU)
+        g(x) = max(0, x)
+      
+  used -> hw(x) = g(w • x)
+
+'''
+
+## ! 8:55
